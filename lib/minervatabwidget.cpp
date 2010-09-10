1ef7d03f903dc4ebaaabcb622e21a6276ebe82b7
Radu Andries <admiral0@tuxfamily.org>
2010-09-10 20:13:37 +0200

    Ui fixes + LICENSE

--- lib/minervatabwidget.cpp
+++ lib/minervatabwidget.cpp
@@ -1,11 +1,15 @@
 #include "minervatabwidget.h"
 #include "minervawindow.h"
-#include <QPushButton>
+#include "minervaconfig.h"
+#include <QToolButton>
 MinervaTabWidget::MinervaTabWidget(QWidget *parent):QTabWidget(parent)
 {
+    qDebug("Creating Tab Widget");
     appSettings=new MinervaSettings();
-    QPushButton *newDoc=new QPushButton("New");
-    QPushButton *settings=new QPushButton("Settings");
+    QToolButton *newDoc=new QToolButton();
+    QToolButton *settings=new QToolButton();
+    newDoc->setIcon(QIcon(QString(ROOTDIR)+"/share/minerva/icons/new.png"));
+    settings->setIcon(QIcon(QString(ROOTDIR)+"/share/minerva/icons/configure.png"));
     setCornerWidget(newDoc,Qt::TopLeftCorner);
     setCornerWidget(settings);
     connect(newDoc,SIGNAL(clicked()),MinervaWindow::instance(),SLOT(newDocument()));
